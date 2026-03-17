bcfe70eb7f53496fed7c3c6155ed67e1e831ea7b
Dohyeop Lim <dhlim0817@gmail.com>
2026-03-17 11:58:53 +0900

    feat: add solutions

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import flutter_local_notifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,10 +8,27 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+        GeneratedPluginRegistrant.register(with: registry)
+    }
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
+
+  @available(iOS 12.0, *)
+  override func userNotificationCenter(
+      _ center: UNUserNotificationCenter,
+      openSettingsFor notification: UNNotification?
+  ) {
+      let controller = window?.rootViewController as! FlutterViewController
+      let channel = FlutterMethodChannel(
+          name: "com.example.noti/settings",
+          binaryMessenger: controller.binaryMessenger)
+
+      channel.invokeMethod("showNotificationSettings", arguments: nil)
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
-void main() {
-  runApp(const MainApp());
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  tz.initializeTimeZones();
+  tz.setLocalLocation(tz.getLocation('Asia/Seoul'));
+
+  const androidSettings = AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  const darwinSettings = DarwinInitializationSettings();
+
+  const initializationSettings = InitializationSettings(
+    android: androidSettings,
+    iOS: darwinSettings,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(
+    settings: initializationSettings,
+  );
+
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+Future<void> showSimpleNotification() async {
+  const androidDetails = AndroidNotificationDetails(
+    'basic_channel',
+    'Basic Notifications',
+    channelDescription: 'Basic noti channel',
+    importance: Importance.max,
+    priority: Priority.high,
+  );
+
+  const notificationDetails = NotificationDetails(
+    android: androidDetails,
+    iOS: DarwinNotificationDetails(),
+  );
+
+  await flutterLocalNotificationsPlugin.show(
+    id: 0,
+    title: 'GDGoC 알림',
+    body: '알림이 도착했습니다!.',
+    payload: 'payload',
+    notificationDetails: notificationDetails,
+  );
+}
+
+Future<void> scheduleNotification() async {
+  const androidDetails = AndroidNotificationDetails(
+    'schedule_channel',
+    'Scheduled Notifications',
+    channelDescription: 'Scheduled noti channel',
+    importance: Importance.max,
+    priority: Priority.high,
+  );
+
+  const notificationDetails = NotificationDetails(
+    android: androidDetails,
+    iOS: DarwinNotificationDetails(),
+  );
+
+  await flutterLocalNotificationsPlugin.zonedSchedule(
+    id: 1,
+    title: 'GDGoC 예약 알림',
+    body: '5초 뒤에 도착한 알림입니다.',
+    payload: 'payload',
+    scheduledDate: tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
+    notificationDetails: notificationDetails,
+    androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Notification Demo',
+      home: const NotificationPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('9주차 세션')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: showSimpleNotification,
+              child: const Text('즉시 알림 보내기'),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: scheduleNotification,
+              child: const Text('5초 뒤 알림 보내기'),
+            ),
+          ],
         ),
       ),
     );
